640254313b9d751518aaceec2de6e759cd3997b2
Rian Ribeiro Rodrigues <rianribeiroprogramador@gmail.com>
2021-09-29 20:38:39 -0300

    aula2

---
+++ src/pages/Home/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { Container, SearchContainer, Input, SearchButton } from "./styles";
+import { Feather } from "@expo/vector-icons";
+
+import Header from "../../components/Header";
+
+function Home() {
+  return (
+    <Container>
+      <Header title="React Prime" />
+
+      <SearchContainer>
+        <Input placeholder="Procurando algo?" placeholderTextColor="#DDD" />
+
+        <SearchButton>
+          <Feather name="search" size={30} color="#FFF" />
+        </SearchButton>
+      </SearchContainer>
+    </Container>
+  );
+}
+
+export default Home;
